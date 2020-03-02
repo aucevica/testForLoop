95df650b011d83f52de295febfdb981bf251d83a
aucevica <aucevica@gmail.com>
2020-03-02 15:52:39 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'testForLoop';
+
+count = 0;
+
+products = ['product #1','product #2','product #3','product #4',]
+
+ngOnInit() {
+  console.log(this.count);
+}
+
+func(product: any){
+  console.log ('Product =>',product,' COUNT =>', this.count++);
+   
+}
+
+
+
 }
